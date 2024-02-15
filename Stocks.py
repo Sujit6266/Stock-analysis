246b1d846f2924b77bede40f477e5c2d8443d5c4
Sujit6266 <159415658+Sujit6266@users.noreply.github.com>
2024-02-15 13:04:30 +0530

    Update Stocks.py

--- Stocks.py
+++ Stocks.py
@@ -36,7 +36,7 @@ with pricing_data:
 
 from alpha_vantage.fundamentaldata import FundamentalData
 with fundamental_data:
-    key = 'OPJF0JNRIPA233ZI'
+    key = 'A4CNYS532QW69PA9'
     fd = FundamentalData(key, output_format='pandas')
     st.subheader('Balance Sheet')
     balance_sheet = fd.get_balance_sheet_annual(ticker)[0]
